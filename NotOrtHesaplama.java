98d278d38493b5f5c557d43edabadbb0c364bc7f
esaricoglu <84877085+esaricoglu@users.noreply.github.com>
2023-01-20 20:35:47 +0300

    Update NotOrtHesaplama.java
    System.out.println() yerine \n eklendi.

--- NotOrtHesaplama.java
+++ NotOrtHesaplama.java
@@ -8,21 +8,26 @@ public class NotOrtHesaplama {
 
         System.out.print("Matematik notunuzu giriniz : ");
         matematik = input.nextInt();
+        
         System.out.print("Fizik notunuzu giriniz : ");
         fizik = input.nextInt();
+        
         System.out.print("Kimya notunuzu giriniz : ");
         kimya = input.nextInt();
+        
         System.out.print("Türkçe notunuzu giriniz : ");
         turkce = input.nextInt();
+        
         System.out.print("Tarih notunuzu giriniz : ");
         tarih = input.nextInt();
+        
         System.out.print("Müzik notunuzu giriniz : ");
         muzik = input.nextInt();
 
         double ort = (matematik+fizik+kimya+turkce+tarih+muzik)/6.0;
-        System.out.println();
-        System.out.println("Not Ortalamanız = " + ort);
-        System.out.println();
+        
+        System.out.println("\nNot Ortalamanız = " + ort);
+        
         String gectikaldi = ort>=60 ? "Sınıfı geçti" : "Sınıfta kaldı";
         System.out.println(gectikaldi);
 
